3015f9ef729d93dffe4d5ce27c6e74caab5f3c77
Adam Gessel <damageless@gmail.com>
2018-02-11 19:49:36 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/SlackKit/SKCore", majorVersion: 4),
-        .Package(url: "https://github.com/SlackKit/SKWebAPI", majorVersion: 4),
+        .Package(url: "https://github.com/damageless/SKWebAPI", majorVersion: 4),
         .Package(url: "https://github.com/httpswift/swifter.git", "1.3.3")
     ]
 )
